2c33a2a7da732bbf5e39de3777a2a16ccbbddd07
LUIS ANGEL ALMAZAN DE JESUS <147682457+LuisAngelAlmazan@users.noreply.github.com>
2025-02-05 12:02:27 -0600

    seguimos con estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 
-
-export default function Header({ cart,deleteProduc,incrementProdutc,decrementProdutc,clearCart,isEmpy,cartTotal }) {
-
-
+export default function Header({
+  cart,
+  deleteProduc,
+  incrementProdutc,
+  decrementProdutc,
+  clearCart,
+  isEmpy,
+  cartTotal,
+}) {
   return (
     <header className="py-5 header">
       <div className="container-xl">
@@ -16,17 +21,16 @@ export default function Header({ cart,deleteProduc,incrementProdutc,decrementPro
               />
             </a>
           </div>
-          <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+          <nav className="col-md-6 d-flex align-items-center justify-content-end">
             <div className="carrito">
               <img
                 className="img-fluid"
                 src="img/carrito.png"
                 alt="imagen carrito"
               />
-
               <div id="carrito" className="bg-white p-3">
                 {isEmpy ? (
-                  <p className="text-center">El carrito esta vacio</p>
+                  <p className="text-center">El carrito está vacío</p>
                 ) : (
                   <>
                     <table className="w-100 table">
@@ -51,26 +55,29 @@ export default function Header({ cart,deleteProduc,incrementProdutc,decrementPro
                             </td>
                             <td>{guitar.name}</td>
                             <td className="fw-bold">${guitar.price}</td>
-                            <td className="flex align-items-start gap-4">
+                            <td className="d-flex align-items-start gap-4">
                               <button 
-                              type="button" 
-                              className="btn btn-dark"
-                              onClick={()=>decrementProdutc(guitar.id)}>
+                                type="button" 
+                                className="btn btn-dark"
+                                onClick={() => decrementProdutc(guitar.id)}
+                              >
                                 -
                               </button>
                               {guitar.quantity}
                               <button 
-                              type="button" 
-                              className="btn btn-dark"
-                              onClick={()=>incrementProdutc(guitar.id)}>
+                                type="button" 
+                                className="btn btn-dark"
+                                onClick={() => incrementProdutc(guitar.id)}
+                              >
                                 +
                               </button>
                             </td>
                             <td>
                               <button 
-                              className="btn btn-danger" 
-                              type="button"
-                              onClick={()=>deleteProduc(guitar.id)}>
+                                className="btn btn-danger" 
+                                type="button"
+                                onClick={() => deleteProduc(guitar.id)}
+                              >
                                 X
                               </button>
                             </td>
@@ -78,15 +85,15 @@ export default function Header({ cart,deleteProduc,incrementProdutc,decrementPro
                         ))}
                       </tbody>
                     </table>
-
                     <p className="text-end">
                       Total pagar: <span className="fw-bold">${cartTotal}</span>
                     </p>
                   </>
                 )}
                 <button 
-                className="btn btn-dark w-100 mt-3 p-2"
-                onClick={clearCart}>
+                  className="btn btn-dark w-100 mt-3 p-2"
+                  onClick={clearCart}
+                >
                   Vaciar Carrito
                 </button>
               </div>
